0dc4c31b9a94b86568580c8b65b017e73e672a7d
ANDREI AHUREIKIN <ahureikinandrei@gmail.com>
2020-03-12 18:57:36 +0300

    feat: add solution fot dream-team task

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,4 +1,19 @@
-module.exports = function createDreamTeam(/* members */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
-};
+module.exports = function createDreamTeam(members) {
+  let team = [];
+
+  if (Array.isArray(members)) { 
+ 
+    for (let j = 0; j < members.length; j++) {
+      if (typeof members[j] == 'string'){
+      members[j] = members[j].trim();
+      }
+    }
+    for (let k = 0; k < members.length; k++) {
+      
+      if (typeof members[k] == 'string') {
+      team.push(members[k][0].toUpperCase());
+      }
+    }
+    return team.sort().join('');
+  } else return false;
+} ;
