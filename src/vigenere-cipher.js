26b467226e54f73148ff39221faad3d0e7781e48
ANDREI AHUREIKIN <ahureikinandrei@gmail.com>
2020-09-27 21:26:17 +0300

    feat: add solution for extended and vigenere

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,12 +1,37 @@
 class VigenereCipheringMachine {
-    encrypt() {
-        throw 'Not implemented';
-        // remove line with error and write your code here
+    constructor(direct = true) {
+        this.direct = direct;
     }
 
-    decrypt() {
-        throw 'Not implemented';
-        // remove line with error and write your code here
+    encrypt(message, key) {
+        const alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+        const arr = [];
+        let keyStep = -1;
+        for (let i = 0; i < message.length; i++) {
+            if (alphabet.indexOf(message[i].toUpperCase()) > -1) {
+                keyStep++;
+                let number = (alphabet.indexOf(message[i].toUpperCase()) + alphabet.indexOf(key[keyStep % key.length].toUpperCase())) % alphabet.length;
+                arr.push(alphabet[number]);
+            } else {
+                arr.push(message[i])
+            }
+        }
+        return this.direct ? arr.join('') : arr.reverse().join('');
+    }
+    decrypt(encryptedMessage, key) {
+        const alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+        const arr = [];
+        let keyStep = -1;
+        for (let i = 0; i < encryptedMessage.length; i++) {
+            if (alphabet.indexOf(encryptedMessage[i].toUpperCase()) > -1) {
+                keyStep++;
+                let number = (alphabet.indexOf(encryptedMessage[i].toUpperCase()) + alphabet.length - alphabet.indexOf(key[keyStep % key.length].toUpperCase())) % alphabet.length;
+                arr.push(alphabet[number])
+            } else {
+                arr.push(encryptedMessage[i])
+            }
+        }
+        return this.direct ? arr.join('') : arr.reverse().join('');
     }
 }
 
